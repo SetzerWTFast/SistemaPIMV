765319e0a0202fd7565298615bd7951131d232a0
Nathan Vicente <natavicenteid@hotmail.com>
2020-03-12 02:14:25 -0300

    Criação dos itens da tela de cadsatro de clientes

---
+++ SistemaPIMV/Cadastro/CAD_Produtos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaPIMV.Sistema
+{
+    public partial class CAD_Produtos : Form
+    {
+        public CAD_Produtos()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SistemaPIMV/Consulta/CONS_Produtos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaPIMV.Sistema
+{
+    public partial class CONS_Produtos : Form
+    {
+        public CONS_Produtos()
+        {
+            InitializeComponent();
+        }
+    }
+}
